3f1683d8d1b136bb4c26019edad811f9c4a4114f
Linus <vitocorleone77@gmail.com>
2020-12-02 23:07:08 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kinderkultur_WebApiClient
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            await ProcessRepositories();
+        }
+
+        private static async Task ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/x-www-form-urlencode"));
+
+            var url = "https://accounts.ticketino.com/connect/token";
+
+            var dict = new List<KeyValuePair<string, string>>();       
+            dict.Add(new KeyValuePair<string, string>("grant_type", "password"));
+            dict.Add(new KeyValuePair<string, string>("username", "mariannehofer@kinderkultur.ch"));
+            dict.Add(new KeyValuePair<string, string>("password", "***"));
+            dict.Add(new KeyValuePair<string, string>("scope", "api"));            
+            dict.Add(new KeyValuePair<string, string>("client_id", "public-access-api"));
+            dict.Add(new KeyValuePair<string, string>("client_secret", "secret"));
+
+            HttpResponseMessage response = client.PostAsync(url, new FormUrlEncodedContent(dict)).Result;
+
+            var token= response.Content.ReadAsStringAsync().Result;
+            Console.Write(token);
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,5 @@
+[https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/console-webapiclient](https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/console-webapiclient)
+
+[Basic Authentication](https://gist.github.com/bryanbarnard/8102915)
+
+[Password Auth](https://stackoverflow.com/questions/43158250/how-to-post-using-httpclient-content-type-application-x-www-form-urlencoded)
